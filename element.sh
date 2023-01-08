57bd4999cd8a231955f7929236812c79433e64a8
Andrespeerez <andreslavila93@gmail.com>
2023-01-08 16:16:16 +0000

    feat: read input as symbol and name

--- element.sh
+++ element.sh
@@ -9,9 +9,18 @@ then
 fi
 
 # case not empty
-# try to query by atomic_number
+# try to query by atomic_number, symbol, and name
 ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol='$1'")
+NAME=$($PSQL "SELECT name FROM elements WHERE name='$1'")
 if [[ -z $ATOMIC_NUMBER ]]
 then
-
+  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$1")
+  NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
+else if [[ -z $SYMBOL ]]
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")
+  NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$1'")  
+else if [[ -z $NAME ]]
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
+  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$1'")
 fi
