39855e30ed8d974eca7e07db9a45a024e0267cd3
Andrespeerez <andreslavila93@gmail.com>
2023-01-08 15:57:36 +0000

    feat: input -> case no argument provided

---
+++ element.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script that prints on screen information about a chemical element
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+fi
