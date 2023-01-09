c59e5bbc5196a4a9cd2f162e0929a279ed326477
Andrespeerez <andreslavila93@gmail.com>
2023-01-09 09:34:53 +0000

    fix: element.sh detects all type of inputs

--- element.sh
+++ element.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 # Script that prints on screen information about a chemical element
 PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
-ONE=$1
-# case empty argument
-if [[ -z $ONE ]]
+
+if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+elif [[ $1 =~ ^[0-9]*$ ]]
+then
+  echo "Input is a number"
+elif [[ $1 =~ ^[A-Z].$ ]]
+then
+  echo "Input is a symbol"
+else
+  echo "Input is a name"
 fi
 
-# case not empty
-# try to query by atomic_number, symbol, and name
-ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
-SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol='$1'")
-NAME=$($PSQL "SELECT name FROM elements WHERE name='$1'")
-if [[ -z $ATOMIC_NUMBER ]]
-then
-  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$1")
-  NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
-elif [[ -z $SYMBOL ]]
-  then
-  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")
-  NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$1'")  
-elif [[ -z $NAME ]]
-  then
-  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
-  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$1'")
-else
-  # case input $1 is not found
-  echo "I could not find that element in the database."
-fi
