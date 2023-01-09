caf5b762b1855ac676171ea58766ef084418627e
Andrespeerez <andreslavila93@gmail.com>
2023-01-09 10:46:15 +0000

    feat: added search by atomic_number.

--- element.sh
+++ element.sh
@@ -5,13 +5,49 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-o
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+  exit 0
 elif [[ $1 =~ ^[0-9]*$ ]]
 then
-  echo "Input is a number"
-elif [[ $1 =~ ^[A-Z].$ ]]
+  GET_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+  if [[ -z $GET_ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+    exit 0
+  fi
+  GET_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$1")
+  GET_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
+  GET_ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$1")
+  GET_TYPE=$($PSQL "SELECT type FROM properties INNER JOIN types ON types.type_id = properties.type_id WHERE atomic_number=$1")
+  GET_MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$1")
+  GET_BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$1")
+elif [[ $1 =~ ^[A-Z]$ || $1 =~ ^[A-Z][a-z]$ ]]
 then
-  echo "Input is a symbol"
+  GET_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")
+  if [[ -z $GET_ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+    exit 0
+  fi
+  GET_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol='$1'")
+  GET_NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$1'")
+  GET_ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE symbol='$1'")
+  GET_TYPE=$($PSQL "SELECT type FROM properties INNER JOIN types ON types.type_id = properties.type_id WHERE symbol='$1'")
+  GET_MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE symbol='$1'")
+  GET_BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE symbol='$1'")
 else
-  echo "Input is a name"
+  GET_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
+  if [[ -z $GET_ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+    exit 0
+  fi
+  GET_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$1'")
+  GET_NAME=$($PSQL "SELECT name FROM elements WHERE name='$1'")
+  GET_ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE name='$1'")
+  GET_TYPE=$($PSQL "SELECT type FROM properties INNER JOIN types ON types.type_id = properties.type_id WHERE WHERE name='$1'")
+  GET_MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE name='$1'")
+  GET_BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE name='$1'")
 fi
 
+# send message
+echo "The element with atomic number $GET_ATOMIC_NUMBER is $GET_NAME ($GET_SYMBOL). It's a $GET_TYPE, with a mass of $GET_ATOMIC_MASS amu. $GET_NAME has a melting point of $GET_MP celsius and a boiling point of $GET_BP celsius."
